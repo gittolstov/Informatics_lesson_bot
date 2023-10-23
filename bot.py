869d038fe879a4a3a9b82c941fa6440825e18381
gittolstov <60322564+gittolstov@users.noreply.github.com>
2023-10-23 11:16:52 +0300

    Inline keyboard

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import requests
-from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove, InlineKeyboardButton, InlineKeyboardMarkup
 from random import random
-from telegram.ext import Updater, MessageHandler, CallbackContext, Filters, CommandHandler
+from telegram.ext import Updater, MessageHandler, CallbackContext, Filters, CommandHandler, CallbackQueryHandler
 import logging
 from Token import TOKEN
 
@@ -42,7 +42,7 @@ def calculate(update: Update, context: CallbackContext):
         res = num1 * num2
     elif operation == "/":
         res = num1 / num2
-    update.message.reply_text(f"{num1} {operation} {num2} = {res}.\n")
+    update.message.reply_text(f"{num1} {operation} {num2} = {res}.\n", reply_markup=ReplyKeyboardRemove())
     logger.info(f"user {usr} calculated {num1} {operation} {num2}")
 
 
@@ -55,14 +55,39 @@ def start(update: Update, context: CallbackContext):
         database.join(f"{ident}, ")
 
 
+def inline_keyboard(update: Update, context: CallbackContext):
+    buttons = [
+        ["1", "2", "3"],
+        ["4", "5", "6"],
+        ["7", "8", "9"],
+    ]
+    for a in range(len(buttons)):
+        for b in range(len(buttons[a])):
+            buttons[a][b] = InlineKeyboardButton(text=buttons[a][b], callback_data=f"{buttons[a][b]}_{a}_{b}")
+    keyboard = InlineKeyboardMarkup(buttons)
+    logger.info("Inline keyboard created")
+    update.message.reply_text("Choose wisely", reply_markup=keyboard)
+
+
+def inline_keyboard_button_handler(update: Update, context: CallbackContext):
+    query = update.callback_query
+    logger.info(query.data)
+    logger.info(query.message.reply_markup.inline_keyboard)
+    keyboard = query.message.reply_markup.inline_keyboard
+    data = query.data.split("_")
+    keyboard[int(data[1])][int(data[2])] = InlineKeyboardButton(text=data[0], callback_data=f"{int(data[0]) + 1}_{data[1]}_{data[2]}")
+    #query.edit_message_text("aboba", reply_markup=InlineKeyboardMarkup(keyboard))
+
+
 def keyboard_handler(update: Update, context: CallbackContext):
     buttons = [
         ["один", "два", "три"],
-        [ "четыре", "пять", "шесть"],
-        [ "семь", "восемь", "одиннадцать"],
+        ["четыре", "пять", "шесть"],
+        ["семь", "восемь", "одиннадцать"],
     ]
     keyboard = ReplyKeyboardMarkup(buttons, resize_keyboard=True)
     update.message.reply_text("Choose wisely", reply_markup=keyboard)
+    logger.info("Keyboard created")
 
 
 def v1_text(update: Update, context: CallbackContext):
@@ -73,14 +98,13 @@ def v1_text(update: Update, context: CallbackContext):
 def main():
     updater = Updater(TOKEN)
     dispatcher = updater.dispatcher
-    start_handler = CommandHandler("start", start)
-    dispatcher.add_handler(start_handler)
-    start_handler = CommandHandler("prepare_thyself", v1_text)
-    dispatcher.add_handler(start_handler)
-    keyb_handler = CommandHandler("keyboard", keyboard_handler)
-    dispatcher.add_handler(keyb_handler)
+    command_list = ["start", "prepare_thyself", "keyboard"]
+    function_list = [start, v1_text, inline_keyboard]
+    for i in range(len(command_list)):
+        dispatcher.add_handler(CommandHandler(command_list[i], function_list[i]))
     echo_handler = MessageHandler(Filters.text & (~Filters.command), calculate)
     dispatcher.add_handler(echo_handler)
+    dispatcher.add_handler(CallbackQueryHandler(inline_keyboard_button_handler))
     updater.start_polling()
     updater.idle()
 
